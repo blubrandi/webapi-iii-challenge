210aeb361a6aa56bd5be4c2bfb4b8163af4a233c
blubrandi <blubrandi0@gmail.com>
2019-04-24 22:02:56 -0600

    Added get posts by userId

--- server.js
+++ server.js
@@ -1,92 +1,110 @@
-const express = require('express')
+const express = require("express");
 
-const posts = require('./data/helpers/postDb')
-const users = require('./data/helpers/userDb')
+const posts = require("./data/helpers/postDb");
+const users = require("./data/helpers/userDb");
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-
-server.get('/', (req, res, next) => {
-    res.send(`
+server.get("/", (req, res, next) => {
+  res.send(`
     <h1>Hello</h1>
-    `)
-})
+    `);
+});
 
 //POSTS
 
 // - GET all posts
 
-server.get('/api/posts', (req, res, next) => {
-    posts
+server.get("/api/posts", (req, res, next) => {
+  posts
     .get()
     .then(posts => {
-        res.json(posts)
+      res.json(posts);
     })
     .catch(error => {
-        res.status(500).json({  error: error, message: 'Posts could not be retrieved.' })
-    })
-})
+      res
+        .status(500)
+        .json({ error: error, message: "Posts could not be retrieved." });
+    });
+});
 
-// - GET all posts by id
+// - GET posts by id
 
-server.get('/api/posts/:id', (req, res, next) => {
-    const { id } = req.params
-    posts
+server.get("/api/posts/:id", (req, res, next) => {
+  const { id } = req.params.id;
+  posts
     .getById(id)
     .then(post => {
-        if (post === 0) {
-            return res(404).json({ message: 'The post you\'re looking for cannot be found.' })
-        }
-        res.json(post)
+      if (post === 0) {
+        return res(404).json({
+          message: "The post you're looking for cannot be found."
+        });
+      }
+      res.json(post);
     })
     .catch(error => {
-        res.status(500).json({  error: error, message: 'Posts could not be retrieved.' })
+      res
+        .status(500)
+        .json({ error: error, message: "Posts could not be retrieved." });
+    });
+});
+
+// GET all posts by user_id
+server.get("/api/posts/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  users
+    .getUserPosts(userId)
+    .then(post => {
+      res.json(post);
     })
-})
+    .catch(error => {
+      res.status(404).json({ error: error, message: "No posts available" });
+    });
+});
 
 // - POST add a post
 
-server.post('/api/posts', (req, res) => {
-    const { user_id, text } = req.body
-    posts
+server.post("/api/posts", (req, res) => {
+  const { user_id, text } = req.body;
+  posts
     .insert({ user_id, text })
     .then(post => {
-        res.json(post)
+      res.json(post);
     })
     .catch(error => {
-        res.status(500).json({  error: error, message: 'Cannot add post' })
-    })
-})
+      res.status(500).json({ error: error, message: "Cannot add post" });
+    });
+});
 
 // - PUT change a post
 
-server.put('/api/posts/:id', (req, res) => {
-    const id = req.params.id
-    const { text } = req.body
-    posts
-    .update( id, { text } )
+server.put("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const { text } = req.body;
+  posts
+    .update(id, { text })
     .then(post => {
-        res.json(post)
+      res.json(post);
     })
     .catch(error => {
-        res.status(500).json({ error: error, message: 'Cannot update post'})
-    })
-})
+      res.status(500).json({ error: error, message: "Cannot update post" });
+    });
+});
 
 // - DELETE remove a post by ID
 
-server.delete('/api/posts/:id', (req, res) => {
-    const id = req.params.id
-    posts
+server.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  posts
     .remove(id)
     .then(post => {
-        res.json({ message: 'Your post has been removed' })
+      res.json({ message: "Your post has been removed" });
     })
     .catch(error => {
-        res.status(500).json({ error: error, message: 'Cannot remove post' })
-    })
-})
+      res.status(500).json({ error: error, message: "Cannot remove post" });
+    });
+});
 
-module.exports = server
+module.exports = server;
